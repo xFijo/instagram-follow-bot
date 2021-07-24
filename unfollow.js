176a7a6562c629fd2d3e25d9c9d667fc69fd9323
xFijo <75216577+xFijo@users.noreply.github.com>
2021-07-25 00:07:29 +0100

    Create unfollow.js

---
+++ unfollow.js
@@ -0,0 +1,26 @@
+// paste this into the console while on the "following tabs" to auto unfollow all the accounts
+
+(() => {
+    let i = 0;
+    let count = 0;
+    const unfollowInterval = setInterval(() => {
+        if (count >= 20) {
+            clearInterval(unfollowInterval);
+            return;
+        }
+        let list = document.querySelectorAll('button');
+        for(let btn of list)
+        {
+            if(btn.innerText === 'Unfollow'){
+                btn.click();
+                console.log('Unfollowed');
+                count++;
+            }
+        }
+        if (list[i].innerText === 'Following') {
+            list[i].click();
+        }
+        i++;
+    }, 4500);
+    console.log("intervalID => ",unfollowInterval);
+})();
